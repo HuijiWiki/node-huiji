2238f0fab4655541c5c73eb0be5db5a8cc1c1309
chenchenyangll <ccy-8888@hotmail.com>
2015-08-04 21:38:18 +0800

    revert useless code in last commit; add _cache_set(_, _, maxAge) to support zh-asoiaf-wiki/zh.asoiaf.wechat#7

--- wechat.js
+++ wechat.js
@@ -9,10 +9,6 @@ module.exports = (function() {
   var API = require('./api.js');
   var api = null;	// API caller
   var cache = null;     // cache
-  var CACHE_USE = 1;
-  var CACHE_NOUSE = 0;  // when the value of a cache entry equals CACHE_NOUSE, 
-                        // wechat will not do cache write for such key anymore 
-                        // it is been set to CACHE_USE later.
   var self = null;	// point to WeChat itself
   var default_conf = {
     port: 80,
@@ -468,29 +464,17 @@ module.exports = (function() {
     /*
      * Write <key, value> into cache
      * if *key* already exists in cache, do not do an additional write.
-     * if such entry is protected by CACHE_NOUSE, do not write neither.
      *
      * Return false if cache is not set or a cache write does not happen.
      * Otherwise, return true if a cache-write succeeds.
      */
-    _cache_set: function(key, value) {
+    _cache_set: function(key, value, maxAge) {
       if (!cache) return false;
-      if (cache.has(key) && cache.peek(key) != CACHE_USE)
-        return false;
+      if (cache.has(key)) return false;
+
+      maxAge = maxAge || this.conf.cache.maxAge;
       console.log('cache write: %s', key);
-      cache.set(key, value);
-      return true;
-    },
-    /*
-     * Force write <key, value> into cache
-     * No matter if it already exists or is protected by CACHE_NOUSE
-     */
-    _cache_write: function(key, value) {
-      if (!cache) return false;
-      if (value == CACHE_NOUSE) console.log('cache protect: %s', key);
-      else if (value == CACHE_USE) console.log('cache unlock: %s', key);
-      else console.log('cache write: %s', key);
-      cache.set(key, value);
+      cache.set(key, value, maxAge);
       return true;
     },
     /*
@@ -635,8 +619,5 @@ module.exports = (function() {
     }
   };
 
-  WeChat.CACHE_USE = CACHE_USE;
-  WeChat.CACHE_NOUSE = CACHE_NOUSE;
-  
   return WeChat;
 }());
